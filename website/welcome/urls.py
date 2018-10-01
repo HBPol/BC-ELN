b83aa475137645f9bbb2963a73a3dc32b52cce9c
hbp_01 <hector.bolsa@outlook.com>
2018-10-01 03:42:11 +0100

    Remove all

--- website/welcome/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-from django.conf.urls.i18n import urlpatterns
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- website/welcome/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from django.template.context_processors import request
-
-def index(request):
-    return HttpResponse ("<h1>This is the welcome page.</h1>")
