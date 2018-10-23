b0b9b9cedd8de44945743c02afe2532324fd7f22
hbp_01 <hector.bolsa@outlook.com>
2018-10-24 00:17:48 +0100

    Data presented in table instead of list. Left/Right sidebars appearance improved.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     
     path('', views.index, name='index'),
+    path('<int:instrument_id>/', views.details, name='details'),
+    
     
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,13 +3,26 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Instruments
 
-# Create your views here.
-
 def index(request):
+    
     page_title = 'Application Index'
     instruments_table = Instruments.objects.all()
+    
     context = {
         'page_title': page_title,
-        'instruments_table': instruments_table
+        'instruments_table': instruments_table,
     }
+    
+    return render(request, 'mainapp/index.html', context)
+
+def details(request):
+    
+    page_title = 'Application Index'
+    instruments_table = Instruments.objects.all()
+    
+    context = {
+        'page_title': page_title,
+        'instruments_table': instruments_table,
+    }
+    
     return render(request, 'mainapp/index.html', context)
